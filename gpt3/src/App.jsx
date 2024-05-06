87ed143c5e66fcefa3cc6d41357d53dca8f1c587
Sanjoul Bhusal <bsanjoul@gmail.com>
2024-05-06 17:31:09 +0545

    Project Setup

---
+++ gpt3/src/App.jsx
@@ -0,0 +1,30 @@
+import "./App.css";
+
+import {
+  Footer,
+  Blog,
+  Possibility,
+  Features,
+  WhatGPT3,
+  Header,
+} from "./containers";
+import { Cta, Brand, Navbar, Article, Feature } from "./components";
+function App() {
+  return (
+    <div className="App">
+      <div className="gradient_bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <Cta />
+      <Blog />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
